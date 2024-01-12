38af3d4d1593327566a261011326cd9b6c91cce4
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-12 10:36:26 +0630

    init

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+  });
+
+  await app.listen(3002).then(() => {
+    console.log('product service running at 3002');
+  });
+}
+bootstrap();
