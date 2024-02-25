3a6cc591161d954b78da263da0638ba0e01a90c8
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-02-25 22:30:48 +0630

    feat: env config using redis cloud server and cockroachDb for deployment

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
-import { env } from './configs/env-config';
+import { env } from './configs/env.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
